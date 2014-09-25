04f4d46ad3cd82041ddbfe964d6115c90cbb25b6
Ambrotos <creanjames@gmail.com>
2014-09-24 22:31:22 -0700

    Possible solution to get string function Taber requested

--- taberSlider.js
+++ taberSlider.js
@@ -4,25 +4,27 @@ var myApp = angular.module('mySlider', []);
 
 myApp.controller('SliderController', ['$scope', function($scope) {
     $scope.value = 5;
-    
+    $scope.string = "----o----";
     $scope.slider = ['-','-','-','-','o','-','-','-','-'];
     $scope.increase = function() {
         $scope.slider[$scope.value - 1] = '-';
         $scope.value = $scope.value + 1;
         $scope.slider[$scope.value - 1] = 'o';
+        $scope.getstring();
     };
 
     $scope.decrease = function() {
         $scope.slider[$scope.value - 1] = '-';
         $scope.value = $scope.value - 1;
         $scope.slider[$scope.value - 1] = 'o';
+        $scope.getstring();
     };
-//    $scope.draw = function() {
-//        var slider = ['-','-','-','-','-','-','-','-','-'];
-//        for(var i = 0; i < 10; i++)
-//        {
-//            
-//        }
-//    };
+    $scope.getstring = function() {  //function takes the slider array and creates a string of the contents. 
+        $scope.string = "";
+        for(var i = 0; i < 9; i++)
+        {
+            $scope.string += $scope.slider[i];  
+        }
+    };
     
 }]);
