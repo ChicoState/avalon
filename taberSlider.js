3126631da4e61e2b3ee527942129e52c0a9a6244
ColinBaldwin <NobleKatana00@gmail.com>
2014-09-28 22:15:16 -0700

    Colin changes: Max and min values
    Set the limiters for the slider

--- taberSlider.js
+++ taberSlider.js
@@ -4,18 +4,24 @@ var myApp = angular.module('mySlider', []);
 
 myApp.controller('SliderController', ['$scope', function($scope) {
     $scope.value = 5;
+    $scope.min = 0;
+    $scope.max = 10;
     
     $scope.slider = ['-','-','-','-','o','-','-','-','-'];
     $scope.increase = function() {
-        $scope.slider[$scope.value - 1] = '-';
-        $scope.value = $scope.value + 1;
-        $scope.slider[$scope.value - 1] = 'o';
+    	if ($scope.value < $scope.max){
+            $scope.slider[$scope.value - 1] = '-';
+            $scope.value = $scope.value + 1;
+            $scope.slider[$scope.value - 1] = 'o';
+    	}
     };
 
     $scope.decrease = function() {
-        $scope.slider[$scope.value - 1] = '-';
-        $scope.value = $scope.value - 1;
-        $scope.slider[$scope.value - 1] = 'o';
+    	if ($scope.value > $scope.min){
+    		$scope.slider[$scope.value - 1] = '-';
+        	$scope.value = $scope.value - 1;
+        	$scope.slider[$scope.value - 1] = 'o';
+    	}
     };
 //    $scope.draw = function() {
 //        var slider = ['-','-','-','-','-','-','-','-','-'];
