34d3859b84ad0e549d8a680eae9bf568f3c228d6
cvang <cvang32@mail.csuchico.edu>
2014-10-01 09:01:48 -0700

    adds keyboard input

--- bossy.slider.js
+++ bossy.slider.js
@@ -1,12 +1,12 @@
 angular.module('app.directive.bossy.slider', [])
-    .controller('SliderController', ['$scope', function($scope) {
+    .controller('SliderController', ['$scope', function ($scope) {
         $scope.value = 5;
         $scope.max = 9;
         $scope.min = 1;
         $scope.string = "----o----";
         $scope.slider = ['-', '-', '-', '-', 'o', '-', '-', '-', '-'];
 
-        $scope.increase = function() {
+        $scope.increase = function () {
             if ($scope.value < $scope.max) {
                 $scope.slider[$scope.value - 1] = '-';
                 $scope.value = $scope.value + 1;
@@ -15,7 +15,20 @@ angular.module('app.directive.bossy.slider', [])
             $scope.getstring();
         };
 
-        $scope.decrease = function() {
+        //This function is to bind the decrease and increase function with the arrow keys
+        $scope.keyBind = function (ev) {
+            $scope.pressed = ev.which;
+            //If arrow key(Left or Down) is pressed then call the decrease() function to decrease the value.
+            if ($scope.pressed == 37 || $scope.pressed == 40) {
+                $scope.decrease();
+            }
+            //same as above but for Up or Right to increase the value.
+            if ($scope.pressed == 38 || $scope.pressed == 39) {
+                $scope.increase();
+            }
+        };
+
+        $scope.decrease = function () {
             if ($scope.value > $scope.min) {
                 $scope.slider[$scope.value - 1] = '-';
                 $scope.value = $scope.value - 1;
@@ -23,19 +36,18 @@ angular.module('app.directive.bossy.slider', [])
             }
             $scope.getstring();
         };
-        $scope.getstring = function() {  //function takes the slider array and creates a string of the contents. 
+        $scope.getstring = function () {  //function takes the slider array and creates a string of the contents. 
             $scope.string = "";
-            for(var i = 0; i < 9; i++)
-            {
-                $scope.string += $scope.slider[i];  
+            for (var i = 0; i < 9; i++) {
+                $scope.string += $scope.slider[i];
             }
         };
-    
+
     }]).directive('bossySlider', function () {
         return {
             restrict: 'AE',
             controller: 'SliderController',
-            template: '<button ng-click="decrease()">-</button><span>{{string}}</span><button ng-click="increase()">+</button><p>The value is {{value}}!</p>',
+            template: '<button ng-click="decrease()" ng-keydown="keyBind($event)">-</button><span>{{string}}</span><button ng-click="increase()" ng-keydown="keyBind($event)">+</button><p>The value is {{value}}!</p>',
             scope: {}
         }
     });
