6c7e6869841daac79466bf636c9c8bb302bc769d
Taber Storm <tfitzgerald3@mail.csuchico.edu>
2014-09-30 17:51:28 -0700

    Correct the merge conflicts between team members
    Colin was working on the limiter at a similar time as James. The end
    result had neither of there work. I merged all there conflicts slider is
    working as desired.

--- taberSlider.js
+++ taberSlider.js
@@ -4,25 +4,32 @@ var myApp = angular.module('mySlider', []);
 
 myApp.controller('SliderController', ['$scope', function($scope) {
     $scope.value = 5;
-    
+    $scope.max = 9;
+    $scope.min = 1;
+    $scope.string = "----o----";
     $scope.slider = ['-','-','-','-','o','-','-','-','-'];
     $scope.increase = function() {
-        $scope.slider[$scope.value - 1] = '-';
-        $scope.value = $scope.value + 1;
-        $scope.slider[$scope.value - 1] = 'o';
+        if ($scope.value < $scope.max) {
+            $scope.slider[$scope.value - 1] = '-';
+            $scope.value = $scope.value + 1;
+            $scope.slider[$scope.value - 1] = 'o';
+        }
+        $scope.getstring();
     };
 
     $scope.decrease = function() {
-        $scope.slider[$scope.value - 1] = '-';
-        $scope.value = $scope.value - 1;
-        $scope.slider[$scope.value - 1] = 'o';
+        if ($scope.value > $scope.min) {
+            $scope.slider[$scope.value - 1] = '-';
+            $scope.value = $scope.value - 1;
+            $scope.slider[$scope.value - 1] = 'o';
+        }
+        $scope.getstring();
     };
-//    $scope.draw = function() {
-//        var slider = ['-','-','-','-','-','-','-','-','-'];
-//        for(var i = 0; i < 10; i++)
-//        {
-//            
-//        }
-//    };
+    $scope.getstring = function () {  //function takes the slider array and creates a string of the contents. 
+        $scope.string = "";
+        for (var i = 0; i < 9; i++) {
+            $scope.string += $scope.slider[i];
+        }
+    };
     
 }]);
