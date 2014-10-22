e0eaeb93b798d07f095aa8fab048151592c27d68
Taber Storm <tfitzgerald3@mail.csuchico.edu>
2014-10-22 12:23:16 -0700

    refactor/ comment
    Made things slightly clearer

--- bossy.slider.js
+++ bossy.slider.js
@@ -1,32 +1,26 @@
 angular.module('app.directive.bossy.slider', [])
-    .controller('SliderController', ['$scope','$sce', function ($scope, $sce) {
+    .controller('SliderController', ['$scope', '$sce', function ($scope, $sce) {
+        //when we can get info into the controller from the directive we need to set or overwrite these when needed
         $scope.value = 5;
         $scope.max = 9;
         $scope.min = 1;
+        //this was roughly based off sumits code. Angular was not letting me display the html object so I made a string of it.
         $scope.barPiece = '<div style="display:inline-block;width:10px;height:10px;background-color:#0000FF;"></div>';
         $scope.slideBut = '<div style="display:inline-block;width:10px;height:10px;background-color:red;"></div>';
+        //this is going to need to be more dynamic and encompassing of all different max and mins
         $scope.slider = [$scope.barPiece, $scope.barPiece, $scope.barPiece, $scope.barPiece, $scope.slideBut, $scope.barPiece, $scope.barPiece, $scope.barPiece, $scope.barPiece];
         $scope.string = $scope.slider.toString().split(",").join("");
-        //$scope.defaults = {
-        //    max: 9,
-        //    min: 1,
-        //    orientation: 'horizantal'
-        //};
+        
+        //our html needs to be trusted to be used in placed in our template
         $scope.renderHtml = function (html_code) {
             return $sce.trustAsHtml(html_code);
         };
         $scope.orientation = 'horizantal';
         
-        //$scope.option = {
-        //    wheels: '3',
-        //    color: 'blue'
-        //};
-        //$scope.option = $scope.setter;
-        //angular.extend($scope.defaults, $scope.option);
-        //angular.extend($scope.sliderDefault, $scope.options);
-        //angular.extend($scope.sliderDefault.orientation, options);
+        
+        //angular.extend();//possibility to be used for setting values
 
-        //checks bounds when attempting to decrease the value
+        //checks bounds when attempting to decrease the value needs to be able to encompass all sorts of starting min values
         $scope.increase = function () {
             if ($scope.value < $scope.max) {
                 $scope.slider[$scope.value - 1] = $scope.barPiece;
@@ -36,7 +30,7 @@ angular.module('app.directive.bossy.slider', [])
             $scope.draw();
         };
 
-        //checks bounds when attempting to decrease the value
+        //checks bounds when attempting to decrease the value needs to be able to encompass all sorts of starting min values
         $scope.decrease = function () {
             if ($scope.value > $scope.min) {
                 $scope.slider[$scope.value - 1] = $scope.barPiece;
@@ -59,7 +53,7 @@ angular.module('app.directive.bossy.slider', [])
             }
         };
         
-        $scope.draw = function () {  //function takes the slider array and creates a string of the contents. 
+        $scope.draw = function () {  //function takes the slider array and creates a string of the contents. Will need a vertical option
             $scope.string = "";
             //changed to the angular forEach loop for readability
             angular.forEach($scope.slider, function (item) {
@@ -68,16 +62,13 @@ angular.module('app.directive.bossy.slider', [])
             return $scope.string;
                 
         };
-        $scope.renderHtml = function (html_code) {
-            return $sce.trustAsHtml(html_code);
-        };
 
     }]).directive('bossySlider', function () {
         return {
             //allows the slider to be created as and attribute or element <bossy-slider><bossy-slider>
             restrict: 'AE',
             controller: 'SliderController',
-            //This is the template the slider form takes and inserts into HTML
+            //This is the template the slider form takes and inserts into HTML we also chang a string into html by making it safe
             template: '<button ng-click="decrease()" ng-keydown="keyBind($event)">-</button><span ng-bind-html="renderHtml(string)">{{}}</span><button ng-click="increase()" ng-keydown="keyBind($event)">+</button><p>The value is {{value}} and orientation is {{options}}!</p>',
             scope: {
                 options: '@options'
