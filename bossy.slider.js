374d56efbe4f6fb6bb4a6778058c7f028085c01f
Taber Storm <tfitzgerald3@mail.csuchico.edu>
2014-11-02 11:09:35 -0800

    the developer choose the max and min values
    Adds features that allow the developer using our directive to set
    specific values and options within our code. Like max value, min value
    and orientation.

--- bossy.slider.js
+++ bossy.slider.js
@@ -1,11 +1,12 @@
 angular.module('app.directive.bossy.slider', [])
     .controller('SliderController', ['$scope', '$sce', function ($scope, $sce) {
 
+
         //we can change these values freely with out fear of breaking aesthetics
-        $scope.max = 15;
-        $scope.min = -5;
-        //button should show up in the middle now or close to if uneven
-        $scope.value = parseInt(($scope.max + $scope.min) / 2);
+        $scope.max = 10;
+        $scope.min = 1;
+        $scope.orientation = "horizontal";
+
 
         //"Art" for a bar piece
         $scope.barPiece = '<div style="display:inline-block;width:10px;height:10px;background-color:#0000FF;"></div>';
@@ -14,30 +15,26 @@ angular.module('app.directive.bossy.slider', [])
         $scope.slideBut = '<div style="display:inline-block;width:10px;height:10px;background-color:red;"></div>';
 
         //as it is named it is used to maintain correct alignment with the slider bar array
-        var offSet = $scope.min - 1;
+        var offSet;
 
         //initialize the slider bar to what ever size we need it uses
-        function makeBar() {
+        $scope.makeBar = function () {
             var constructSlider = [];
             for (var current = $scope.min; current <= $scope.max; current++) {
                 constructSlider.push($scope.barPiece);
             }
+
+            offSet = $scope.min - 1;
+            //button should show up in the middle now or close to if uneven
+            $scope.value = parseInt(($scope.max + $scope.min) / 2);
             constructSlider[$scope.value - ($scope.min)] = $scope.slideBut;
             return constructSlider;
         };
 
-        $scope.slider = makeBar();
-
-        $scope.string = $scope.slider.toString().split(",").join("");
-        
-        //our html needs to be trusted to be used in placed in our template
         $scope.renderHtml = function (html_code) {
             return $sce.trustAsHtml(html_code);
         };
-        $scope.orientation = 'horizantal';
-        
-        
-        //angular.extend();//possibility to be used for setting values
+
 
         //checks bounds when attempting to decrease the value: They Now work well with negitive min values
         $scope.increase = function () {
@@ -71,27 +68,43 @@ angular.module('app.directive.bossy.slider', [])
                 $scope.increase();
             }
         };
-        
-        $scope.draw = function () {  //function takes the slider array and creates a string of the contents. Will need a vertical option
+
+        $scope.draw = function () {  //function takes the slider array and creates a string of the contents. 
             $scope.string = "";
             //changed to the angular forEach loop for readability
             angular.forEach($scope.slider, function (item) {
                 $scope.string += item;
             })
             return $scope.string;
-                
+
         };
-        
+        $scope.renderHtml = function (html_code) {
+            return $sce.trustAsHtml(html_code);
+        };
 
     }]).directive('bossySlider', function () {
         return {
             //allows the slider to be created as and attribute or element <bossy-slider><bossy-slider>
             restrict: 'AE',
             controller: 'SliderController',
-            //This is the template the slider form takes and inserts into HTML we also chang a string into html by making it safe
-            template: '<button ng-click="decrease()" ng-keydown="keyBind($event)">-</button><span ng-bind-html="renderHtml(string)">{{}}</span><button ng-click="increase()" ng-keydown="keyBind($event)">+</button><input type= "text" ng-model= "max">max</input><input type= "text" ng-model= "min">min</input><p>The value is {{value}} and orientation is {{options}}!</p>',
+            //This is the template the slider form takes and inserts into HTML
+            template: '<button ng-click="decrease()" ng-keydown="keyBind($event)">-</button><span ng-bind-html="renderHtml(string)"></span><button ng-click="increase()" ng-keydown="keyBind($event)">+</button><p>The value is {{value}} and orientation is {{orientation}}!</p>',
             scope: {
-                options: '@'
+                ngModel: '='
+            },
+            link: function (scope, iElem, iAttr) {
+                if (iAttr.max) {
+                    scope.max = parseInt(iAttr.max);
+                }
+                if (iAttr.min) {
+                    scope.min = parseInt(iAttr.min);
+                }
+                if (iAttr.orientation) {
+                    scope.orientation = iAttr.orientation;
+                }
+
+                scope.slider = scope.makeBar();
+                scope.draw();
             }
         }
     });
